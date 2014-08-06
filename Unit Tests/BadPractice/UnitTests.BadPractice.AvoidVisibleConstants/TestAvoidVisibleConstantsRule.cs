e2767ce34459ed616e55458b11016639063bf04e
Lee Millward <lee.millward@gmail.com>
2014-08-06 21:46:08 +0100

    Update a few unit tests and reference hint path.

--- Unit Tests/BadPractice/UnitTests.BadPractice.AvoidVisibleConstants/TestAvoidVisibleConstantsRule.cs
+++ Unit Tests/BadPractice/UnitTests.BadPractice.AvoidVisibleConstants/TestAvoidVisibleConstantsRule.cs
@@ -17,7 +17,7 @@ namespace UnitTests.BadPractice.AvoidVisibleConstants
   public class TestAvoidVisibleConstants
   {
     [TestMethod]
-    public void TestVisibleConstantsRuleSimple()
+    public void TestVisibleConstantsRuleProducesExpectedWarnings()
     {
       SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
       @"Public Class Class1
@@ -66,6 +66,33 @@ namespace UnitTests.BadPractice.AvoidVisibleConstants
 
       var messages = context.AnalysisResults.Messages.ToList();
       Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod()]
+    public void TestVisibleConstantsRuleDoesNoIgnoresGeneratedCodeWhenIGCOptionNotSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"<System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+       Public Class Class1
+          Public Const Foo = 42, Bar = 56
+      End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = false },
+                                                    new AnalysisResults(),
+                                                    comp);
+
+      AvoidVisibleConstantFieldRule rule = new AvoidVisibleConstantFieldRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+      Assert.AreEqual("TestFile.vb:3 - 'Public Const' field 'Foo' in type 'Class1' should be declared as 'Shared ReadOnly' instead.", messages[0].ToString());
+      Assert.AreEqual("TestFile.vb:3 - 'Public Const' field 'Bar' in type 'Class1' should be declared as 'Shared ReadOnly' instead.", messages[1].ToString());
     }
   }
 }
--- Unit Tests/BadPractice/UnitTests.BadPractice.VB6ReturnRule/TestVB6ReturnRule.cs
+++ Unit Tests/BadPractice/UnitTests.BadPractice.VB6ReturnRule/TestVB6ReturnRule.cs
@@ -17,7 +17,7 @@ namespace UnitTests.BadPractice.AvoidVisibleConstants
   public class TestVB6ReturnRule
   {
     [TestMethod]
-    public void TestVB6ReturnRuleSimple()
+    public void TestVB6ReturnRuleProducesExpectedWarnings()
     {
       SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
       @"Public Class Class1
@@ -69,6 +69,36 @@ namespace UnitTests.BadPractice.AvoidVisibleConstants
 
       var messages = context.AnalysisResults.Messages.ToList();
       Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod()]
+    public void TestVB6ReturnRuleDoesNotIgnoreGeneratedCodeOnMethodWhenIGCOptionNotSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+            <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+            Public Function Test() As Integer
+                Test = 42
+            End Function
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = false },
+                                                    new AnalysisResults(),
+                                                    comp);
+
+      VB6ReturnRule rule = new VB6ReturnRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(1, messages.Count);
+      Assert.AreEqual("TestFile.vb:5 - Avoid use of VB6 style return statements.", messages[0].ToString());
     }
   }
 }
