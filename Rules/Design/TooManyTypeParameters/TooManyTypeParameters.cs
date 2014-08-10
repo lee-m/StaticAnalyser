ad105139d2b289904a075193ebbc3569fc73538a
Lee Millward <lee.millward@gmail.com>
2014-08-10 23:54:47 +0100

    Add a rule for types with too many type parameters.

---
+++ Rules/Design/TooManyTypeParameters/TooManyTypeParameters.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
+using StaticAnalysis.Analysis;
+
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace StaticAnalysis.Rules.Design
+{
+  public class TooManyTypeParameters : TypeAnalysisRule
+  {
+    private const int MaxTypeParamters = 2;
+
+    public override void AnalyseTypeDeclaration(TypeBlockSyntax node, AnalysisContext context, SemanticModel model)
+    {
+      AnalyseType(node, context, model);
+    }
+
+    public override void AnalyseInterfaceDeclaration(TypeBlockSyntax node, AnalysisContext context, SemanticModel model)
+    {
+      AnalyseType(node, context, model);
+    }
+
+    //TODO: combine this
+    private void AnalyseType(TypeBlockSyntax node, AnalysisContext context, SemanticModel model)
+    {
+      INamedTypeSymbol typeSymbol = (INamedTypeSymbol)model.GetDeclaredSymbol(node);
+
+      if (typeSymbol.TypeParameters.Length > MaxTypeParamters)
+        context.AnalysisResults.AddWarning(node.GetLocation(),
+                                           "Avoid excessive type parameters to {0} '{1}'.",
+                                           typeSymbol.TypeKind == TypeKind.Interface ? "interface" : "type",
+                                           node.Begin.Identifier.Text);
+    }
+  }
+}
---
+++ Unit Tests/Design/UnitTests.Design.TooManyTypeParameters/TestTooManyTypeParameters.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using StaticAnalysis;
+using StaticAnalysis.Analysis;
+using StaticAnalysis.Rules.Design;
+
+using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+
+namespace StaticAnalyser.UnitTests.Design
+{
+  [TestClass]
+  public class TestTooManyTypeParameters
+  {
+    [TestMethod]
+    public void TestTooManyTypeParametersRuleProducesExpectedWarnings()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Interface IFoo(Of T, K, V)
+        End Interface
+
+        Public Class Foo(Of T, K, V)
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      TooManyTypeParameters rule = new TooManyTypeParameters();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+      Assert.AreEqual("TestFile.vb:1 - Avoid excessive type parameters to interface 'IFoo'.", messages[0].ToString());
+      Assert.AreEqual("TestFile.vb:4 - Avoid excessive type parameters to type 'Foo'.", messages[1].ToString());
+    }
+
+    [TestMethod()]
+    public void TestTooManyTypeParametersIgnoresGeneratedCodeWhenIGCOptionSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"<System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+        Public Interface IFoo(Of T, K, V)
+        End Interface
+
+        <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+        Public Class Foo(Of T, K, V)
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = true },
+                                                    new AnalysisResults(),
+                                                    comp);
+
+      TooManyTypeParameters rule = new TooManyTypeParameters();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod()]
+    public void TestTooManyTypeParametersDoesNotIgnoresGeneratedCodeWhenIGCOptionNotSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"<System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+        Public Interface IFoo(Of T, K, V)
+        End Interface
+
+        <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+        Public Class Foo(Of T, K, V)
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = false },
+                                                    new AnalysisResults(),
+                                                    comp);
+
+      TooManyTypeParameters rule = new TooManyTypeParameters();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+      Assert.AreEqual("TestFile.vb:1 - Avoid excessive type parameters to interface 'IFoo'.", messages[0].ToString());
+      Assert.AreEqual("TestFile.vb:5 - Avoid excessive type parameters to type 'Foo'.", messages[1].ToString());
+    }
+
+    [TestMethod()]
+    public void TestTooManyTypeParametersDoesNotWarnForOneOrTwoTypeParams()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Interface IFoo(Of T)
+        End Interface
+
+        Public Interface IBar(Of T, K)
+        End Interface
+
+        Public Class Foo(Of T)
+        End Class
+
+        Public Class Bar(Of T, K)
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = false },
+                                                    new AnalysisResults(),
+                                                    comp);
+
+      TooManyTypeParameters rule = new TooManyTypeParameters();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+  }
+}
