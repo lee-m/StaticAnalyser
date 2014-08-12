6495b1a905e06abc0cd728c4de5567de464c6730
Lee Millward <lee.millward@gmail.com>
2014-08-12 15:23:56 +0100

    Add a rule checking for unsealed attributes.

---
+++ Rules/Performance/UnsealedAttribute/UnsealedAttributeRule.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
+using StaticAnalysis.Analysis;
+
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace StaticAnalysis.Rules.Performance
+{
+  [Export(typeof(AnalysisRuleBase))]
+  public class UnsealedAttributeRule : TypeAnalysisRule
+  {
+    public override void AnalyseTypeDeclaration(TypeBlockSyntax node, AnalysisContext context, SemanticModel model)
+    {
+      //If the type doesn't inherit from anything it can't be an attribute
+      INamedTypeSymbol typeSymbol = (INamedTypeSymbol)model.GetDeclaredSymbol(node);
+
+      if (typeSymbol.BaseType == null)
+        return;
+
+      //If it's sealed, we can skip it
+      if (typeSymbol.IsSealed)
+        return;
+
+      //If the type doesn't inherit from System.Attribute, walk the inheritance train to see if
+      //it derives from a type that does inherit from System.Attribute
+      bool isAttribute = false;
+      INamedTypeSymbol baseType = typeSymbol.BaseType;
+      string attributeClassName = typeof(Attribute).FullName;
+
+      while(baseType != null)
+      {
+        if (baseType.ToString() == attributeClassName)
+        {
+          isAttribute = true;
+          break;
+        }
+        else
+          baseType = baseType.BaseType;
+      }
+
+      if (!isAttribute)
+        return;
+
+      //If we get here it's an unsealed attribute so warn
+      context.AnalysisResults.AddWarning(node.GetLocation(),
+                                         "Attribute '{0}' should be declared as 'NotInheritable'.",
+                                         typeSymbol.Name);
+    }
+  }
+}
---
+++ UnitTests.Performance.UnsealedAttribute/TestUnsealedAttributeRule.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using StaticAnalysis;
+using StaticAnalysis.Analysis;
+using StaticAnalysis.Rules.Performance;
+
+using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+
+namespace StaticAnalyser.UnitTests.Performance
+{
+  [TestClass]
+  public class TestUnsealedAttributeRule
+  {
+    [TestMethod]
+    public void TestUnsealedAttributeRuleProducesExpectedWarnings()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class SomeAttribute
+            Inherits System.Attribute
+
+        End Class
+
+        Public Class SomeOtherAttribute
+            Inherits System.ComponentModel.DescriptionAttribute
+
+            Public Sub New()
+                MyBase.New("")
+            End Sub
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(System.ComponentModel.DescriptionAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UnsealedAttributeRule rule = new UnsealedAttributeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+      Assert.AreEqual("TestFile.vb:1 - Attribute 'SomeAttribute' should be declared as 'NotInheritable'.", messages[0].ToString());
+      Assert.AreEqual("TestFile.vb:6 - Attribute 'SomeOtherAttribute' should be declared as 'NotInheritable'.", messages[1].ToString());
+    }
+
+    [TestMethod]
+    public void TestUnsealedAttributeRuleIgnoresGeneratedCodeWhenIGOptionSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"<System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+        Public Class SomeAttribute
+            Inherits System.Attribute
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = true }, new AnalysisResults(), comp);
+
+      UnsealedAttributeRule rule = new UnsealedAttributeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUnsealedAttributeRuleDoesNotIgnoresGeneratedCodeWhenIGOptionNotSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"<System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+        Public Class SomeAttribute
+            Inherits System.Attribute
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = false }, new AnalysisResults(), comp);
+
+      UnsealedAttributeRule rule = new UnsealedAttributeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(1, messages.Count);
+      Assert.AreEqual("TestFile.vb:1 - Attribute 'SomeAttribute' should be declared as 'NotInheritable'.", messages[0].ToString());
+    }
+
+    [TestMethod]
+    public void TestUnsealedAttributeRuleDoesNotWarnTypesNotInheritedFromAttribute()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class SomeType
+            Inherits System.Exception
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = true }, new AnalysisResults(), comp);
+
+      UnsealedAttributeRule rule = new UnsealedAttributeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUnsealedAttributeRuleDoesNotWarnForSealedAttributes()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public NotInheritable Class SomeAttribute
+            Inherits System.Attribute
+
+        End Class
+
+        Public NotInheritable Class SomeOtherAttribute
+            Inherits System.ComponentModel.DescriptionAttribute
+
+            Public Sub New()
+                MyBase.New("")
+            End Sub
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = true }, new AnalysisResults(), comp);
+
+      UnsealedAttributeRule rule = new UnsealedAttributeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUnsealedAttributeRuleDoesNotWarnForTypesWithNoBaseType()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class SomeType
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = true }, new AnalysisResults(), comp);
+
+      UnsealedAttributeRule rule = new UnsealedAttributeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+  }
+}
