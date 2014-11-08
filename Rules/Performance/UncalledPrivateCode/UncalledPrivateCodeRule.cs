4715c7f0a1f3a9b3eaf94fe70c21fd95350bc0a1
Lee Millward <none@none>
2014-11-08 14:21:28 +0000

    Add a new rule which warns about private properties and methods which are never referenced.

---
+++ Rules/Performance/UncalledPrivateCode/UncalledPrivateCodeRule.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.FindSymbols;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
+using StaticAnalysis.Analysis;
+using StaticAnalysis.Analysis.Utils;
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace StaticAnalysis.Rules.Performance
+{
+  [Export(typeof(AnalysisRuleBase))]
+  public class UncalledPrivateCodeRule : TypeAnalysisRule
+  {
+    public override void AnalyseTypeDeclaration(TypeBlockSyntax node, AnalysisContext context, SemanticModel model)
+    {
+      //List of private methods/properties and whether they have been called or not
+      Dictionary<IMethodSymbol, bool> privateMethodMapping = new Dictionary<IMethodSymbol,bool>();
+      Dictionary<IPropertySymbol, bool> privatePropMapping = new Dictionary<IPropertySymbol,bool>();
+
+      //Get a list of private methods within this type
+      var privateMethods = node.DescendantNodes().OfType<MethodBlockSyntax>().Where(method => SyntaxTokenListContainsPrivateKeyword(method.Begin.Modifiers));
+
+      foreach (var privateMethod in privateMethods)
+      {
+        if (context.Options.IgnoreGeneratedCode
+            && AnalysisUtils.HasGeneratedCodeAttribute(privateMethod.Begin.AttributeLists, model))
+          continue;
+
+        var sym = model.GetDeclaredSymbol(privateMethod);
+        privateMethodMapping.Add(sym, false);
+      }
+        
+      //Get details of the private properties. Need to look at property statements to find auto 
+      //implemented and traditional property declarations
+      var props = node.DescendantNodes().OfType<PropertyStatementSyntax>();
+      props = props.Where(prop => SyntaxTokenListContainsPrivateKeyword(prop.Modifiers));
+
+      foreach (var property in props)
+      {
+        if (context.Options.IgnoreGeneratedCode
+            && AnalysisUtils.HasGeneratedCodeAttribute(property.AttributeLists, model))
+          continue;
+
+        privatePropMapping.Add((IPropertySymbol)model.GetDeclaredSymbol(property), false);
+      }
+
+      //Find all invokcation statements contained within this type and determine if they call any private method
+      foreach(var invokeExpr in node.DescendantNodes().OfType<InvocationExpressionSyntax>())
+      {
+        var calledMethod = model.GetSymbolInfo(invokeExpr.Expression);
+        ISymbol symbol = null;
+
+        if (calledMethod.Symbol != null)
+          symbol = calledMethod.Symbol;
+        else if (calledMethod.CandidateSymbols.Length == 1)
+          symbol = calledMethod.CandidateSymbols[0];
+
+        if(symbol != null
+           && symbol.Kind == SymbolKind.Method)
+        {
+          IMethodSymbol methodSym = (IMethodSymbol)symbol;
+
+          if(methodSym.DeclaredAccessibility == Accessibility.Private)
+            privateMethodMapping[methodSym] = true;
+        }
+      }
+
+      //Find all references to symbols within this type and see if they refer to a private property
+      foreach(var identifierNode in node.DescendantNodes().OfType<IdentifierNameSyntax>())
+      {
+        SymbolInfo identifierSymbol = model.GetSymbolInfo(identifierNode);
+        IPropertySymbol propSymbol = identifierSymbol.Symbol as IPropertySymbol;
+
+        if (propSymbol != null
+           && privatePropMapping.ContainsKey(propSymbol))
+          privatePropMapping[propSymbol] = true;
+      }
+
+      //Check for any private methods which have their address taken
+      foreach(var unaryExpr in node.DescendantNodes().OfType<UnaryExpressionSyntax>())
+      {
+        //Only interested in AddressOf expressions
+        if (unaryExpr.OperatorToken.VisualBasicKind() != SyntaxKind.AddressOfKeyword)
+          continue;
+
+        SymbolInfo delegateSym = model.GetSymbolInfo(unaryExpr.Operand);
+        IMethodSymbol methodSym = delegateSym.Symbol as IMethodSymbol;
+
+        if (methodSym != null
+           && privateMethodMapping.ContainsKey(methodSym))
+          privateMethodMapping[methodSym] = true;
+      }
+
+      //Issue warnings for any uncalled private methods
+      foreach(var privateMethodItem in privateMethodMapping)
+      {
+        if(!privateMethodItem.Value)
+          context.AnalysisResults.AddWarning(privateMethodItem.Key.Locations.First(), 
+                                             "Private method '{0}' is never referenced.", 
+                                             privateMethodItem.Key.Name);
+      }
+
+      //Issue warnings for any uncalled private properties
+      foreach (var privatePropItem in privatePropMapping)
+      {
+        if (!privatePropItem.Value)
+          context.AnalysisResults.AddWarning(privatePropItem.Key.Locations.First(),
+                                             "Private property '{0}' is never referenced.",
+                                             privatePropItem.Key.Name);
+      }
+    }
+
+    private bool SyntaxTokenListContainsPrivateKeyword(SyntaxTokenList list)
+    {
+      foreach (SyntaxToken tok in list)
+      {
+        if (tok.VisualBasicKind() == SyntaxKind.PrivateKeyword)
+          return true;
+      }
+
+      return false;
+    }
+  }
+}
---
+++ Unit Tests/Performance/UnitTests.Performance.UncalledPrivateCode/TestUncalledPrivateCodeRule.cs
@@ -0,0 +1,447 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.VisualBasic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using StaticAnalysis;
+using StaticAnalysis.Analysis;
+using StaticAnalysis.Rules.Performance;
+
+using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+
+namespace StaticAnalyser.UnitTests.Performance
+{
+  [TestClass]
+  public class TestUncalledPrivateCodeRule
+  {
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleWarnsForUncalledPrivateMethod()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Private Sub UncalledSub()
+          End Sub
+
+          Private Function UncalledMethod() As Integer
+              Return 0
+          End Function
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForGeneratedUncalledPrivateMethodWhenIGCSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private Sub UncalledSub()
+          End Sub
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private Function UncalledMethod() As Integer
+              Return 0
+          End Function
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = true }, new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleWarnsForGeneratedUncalledPrivateMethodWhenIGCNotSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private Sub UncalledSub()
+          End Sub
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private Function UncalledMethod() As Integer
+              Return 0
+          End Function
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = false }, new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleWarnsForUncalledPrivateProperty()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Private Property PrivateProperty As Integer
+
+          Private ReadOnly Property PrivateReadOnlyProperty
+              Get
+                  Return 0
+              End Get
+          End Property
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForUncalledGeneratedPrivatePropertyWhenIGCSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private Property PrivateProperty As Integer
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private ReadOnly Property PrivateReadOnlyProperty
+              Get
+                  Return 0
+              End Get
+          End Property
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = true }, new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleWarnsForUncalledGeneratedPrivatePropertyWhenIGCNotSet()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private Property PrivateProperty As Integer
+
+          <System.CodeDom.Compiler.GeneratedCode(""Tool"", ""1.0.0.0"")>
+          Private ReadOnly Property PrivateReadOnlyProperty
+              Get
+                  Return 0
+              End Get
+          End Property
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddReferences(new MetadataFileReference(typeof(GeneratedCodeAttribute).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions() { IgnoreGeneratedCode = false }, new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(2, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForCalledPrivateMethod()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Public Sub New()
+              
+              CalledSub()
+              Call CalledMethod()
+
+              If CalledMethod2() > 1 Then
+              End If
+              
+              If CalledMethod3(False) Then
+              End If
+
+          End Sub
+
+          Private Sub CalledSub()
+          End Sub
+
+          Private Function CalledMethod() As Integer
+              Return 0
+          End Function
+
+          Private Function CalledMethod2() As Integer
+              Return 0
+          End Function
+
+          Private Function CalledMethod3(param As Boolean) As Boolean
+            Return False
+          End Function
+
+        End Class", "Test.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForCalledPrivateProperty()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Public Sub New()
+              Dim a = PrivateProperty
+              Dim b = PrivateReadOnlyProperty
+          End Sub
+
+          Private Property PrivateProperty As Integer
+
+          Private ReadOnly Property PrivateReadOnlyProperty
+              Get
+                  Return 0
+              End Get
+          End Property
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForUncalledProtectedMethod()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Protected Sub UncalledSub()
+          End Sub
+
+          Protected Friend Sub UncalledFriendSub()
+          End Sub
+
+          Protected Function UncalledMethod() As Integer
+              Return 0
+          End Function
+
+          Protected Friend Function UncalledFriendMethod() As Integer
+              Return 0
+          End Function
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForUncalledProtectedProperty()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Protected Property ProtectedProperty As Integer
+
+          Protected ReadOnly Property ProtectedReadOnlyProperty
+              Get
+                  Return 0
+              End Get
+          End Property
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForUncalledPublicMethod()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Public Sub PublicSub()
+          End Sub
+
+          Public Function PublicMethod() As Integer
+              Return 0
+          End Function
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForUncalledPublicProperty()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Public Property PublicProperty As Integer
+
+          Public ReadOnly Property PublicReadOnlyProperty
+              Get
+                  Return 0
+              End Get
+          End Property
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+
+    [TestMethod]
+    public void TestUncalledPrivateCodeRuleRuleDoesNotWarnForUncalledPrivateMethodPassedToAddHandler()
+    {
+      SyntaxTree syntaxTree = VisualBasicSyntaxTree.ParseText(
+      @"Public Class Class1
+
+          Public Event AnEvent()
+          Public Event AnotherEvent()
+
+          Private Sub SomeEventHandler()
+          End Sub
+
+          Private Sub AnotherEventHandler()
+          End Sub
+
+          Public Sub Test()
+
+            Dim c1 As New Class1 
+            AddHandler c1.AnEvent, AddressOf SomeEventHandler
+  
+            Dim handler As AnotherEventEventHandler = AddressOf AnotherEventHandler
+            AddHandler c1.AnotherEvent, handler
+            
+          End Sub          
+
+        End Class", "TestFile.vb");
+
+      Compilation comp = VisualBasicCompilation.Create("Test")
+                         .AddReferences(new MetadataFileReference(typeof(Object).Assembly.Location))
+                         .AddSyntaxTrees(syntaxTree);
+      SemanticModel model = comp.GetSemanticModel(syntaxTree);
+      AnalysisContext context = new AnalysisContext(new AnalysisOptions(), new AnalysisResults(), comp);
+
+      UncalledPrivateCodeRule rule = new UncalledPrivateCodeRule();
+      rule.ExecuteRuleAsync(context).Wait();
+
+      var messages = context.AnalysisResults.Messages.ToList();
+      Assert.AreEqual(0, messages.Count);
+    }
+  }
+}
