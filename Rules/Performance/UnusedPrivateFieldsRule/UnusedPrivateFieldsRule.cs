90f9ec428f33a28970843b5bbc5380e248f7eda8
Lee Millward <none@none>
2017-06-14 21:44:33 +0100

    Latest work on unused private fields rule.

--- Rules/Performance/UnusedPrivateFieldsRule/UnusedPrivateFieldsRule.cs
+++ Rules/Performance/UnusedPrivateFieldsRule/UnusedPrivateFieldsRule.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.FindSymbols;
 using Microsoft.CodeAnalysis.VisualBasic;
 using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 
@@ -7,6 +8,7 @@ using StaticAnalysis.Analysis.Utils;
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,57 +19,12 @@ namespace StaticAnalysis.Rules.Performance
   /// Determines whether a class or structure declaration contains any private fields which are never used.
   /// </summary>
   [Export(typeof(AnalysisRuleBase))]
-  public class UnusedPrivateFieldsRule : AnalysisRuleBase
+  public class UnusedPrivateFieldsRule : TypeAnalysisRule
   {
-    public override async Task ExecuteRuleAsync(AnalysisContext context)
-    {
-      Dictionary<INamedTypeSymbol, Dictionary<IFieldSymbol, bool>> fieldDets;
-      fieldDets = new Dictionary<INamedTypeSymbol, Dictionary<IFieldSymbol, bool>>();
-
-      foreach (var tree in context.CurrentCompilation.SyntaxTrees)
-      {
-        SemanticModel model = context.CurrentCompilation.GetSemanticModel(tree);
-        CompilationUnitSyntax compilationUnit = (CompilationUnitSyntax)await tree.GetRootAsync();
-
-        //Get a list of type within this compilation unit
-        var typeDecls = compilationUnit.DescendantNodes().OfType<TypeBlockSyntax>();
-
-        foreach (TypeBlockSyntax typeDecl in typeDecls)
-        {
-          if (context.Options.IgnoreGeneratedCode
-              && AnalysisUtils.HasGeneratedCodeAttribute(typeDecl.Begin.AttributeLists, model))
-            continue;
-
-          INamedTypeSymbol typeSymbol = model.GetDeclaredSymbol(typeDecl);
-
-          if (!fieldDets.ContainsKey(typeSymbol))
-            fieldDets.Add(typeSymbol, new Dictionary<IFieldSymbol, bool>());
-
-          await Task.Run(() => AnalyseTypeDeclaration(typeDecl, typeSymbol, context, model, fieldDets[typeSymbol]));
-        }
-      }
-
-      //Warn for any field which isn't referenced. Need to wait until the end to handle partial
-      //classes where a field may be referenced in a different compilation unit
-      foreach (INamedTypeSymbol typeSymbol in fieldDets.Keys)
-      {
-        foreach (var field in fieldDets[typeSymbol])
-        {
-          if (!field.Value)
-            context.AnalysisResults.AddWarning(field.Key.Locations.First(),
-                                       "Field '{0}' within type '{1}' is never referenced.",
-                                       field.Key.Name, typeSymbol.Name);
-        }
-      }
-    }
-
-    private void AnalyseTypeDeclaration(TypeBlockSyntax typeDecl,
-                                        INamedTypeSymbol typeSymbol,
-                                        AnalysisContext context,
-                                        SemanticModel model,
-                                        Dictionary<IFieldSymbol, bool> fieldMapping)
+    public override void AnalyseTypeDeclaration(TypeBlockSyntax node, AnalysisContext context, SemanticModel model)
     {
       //Get the list of fields for this type
+      ITypeSymbol typeSymbol = model.GetDeclaredSymbol(node);
       IEnumerable<IFieldSymbol> typeFields = typeSymbol.GetMembers().OfType<IFieldSymbol>();
 
       //Filter out any fields which are not declared private and are not implicitly declared
@@ -76,35 +33,18 @@ namespace StaticAnalysis.Rules.Performance
                                                     && !field.IsImplicitlyDeclared
                                                  select field);
 
-      //For partial classes, we see the type multiple times (once for each partial) so we may
-      //already have added some fiels for this type
+      //Look for any reference in any source file for this type (partial types)
+      var documents = typeSymbol.Locations.Select(loc => context.Project.GetDocument(loc.SourceTree)).ToImmutableHashSet();
+
       foreach (IFieldSymbol privateField in privateFields)
       {
-        if (!fieldMapping.ContainsKey(privateField))
-          fieldMapping.Add(privateField, false);
+        var lookupResults = SymbolFinder.FindReferencesAsync(privateField, context.Solution, documents).Result;
+        
+        if(!lookupResults.Any(res => res.Locations.Any()))
+          context.AnalysisResults.AddWarning(privateField.Locations.First(),
+                                           "Field '{0}' within type '{1}' is never referenced.",
+                                           privateField.Name, typeSymbol.Name);
       }
-
-      //Make a pass over the identifiers referred to by this type to determine if they in turn
-      //refer to a private field
-      var identifiers = typeDecl.DescendantNodes().OfType<IdentifierNameSyntax>();
-
-      foreach (IdentifierNameSyntax identifier in identifiers)
-      {
-        SymbolInfo identifierSymbol = model.GetSymbolInfo(identifier);
-        IFieldSymbol fieldSymbol = identifierSymbol.Symbol as IFieldSymbol;
-
-        //Check that this symbol refers to a field in the current type
-        if (fieldSymbol != null
-            && identifierSymbol.Symbol.ContainingType == typeSymbol)
-          fieldMapping[fieldSymbol] = true;
-      }
-    }
-
-    protected override Task AnalyseCompilationUnitAsync(CompilationUnitSyntax compilationUnit,
-                                                        SemanticModel model,
-                                                        AnalysisContext context)
-    {
-      throw new NotSupportedException();
     }
   }
 }
