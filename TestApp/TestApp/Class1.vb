17480a2cde936b4f3b8d23b00fd5f2fbf1066d96
Lee Millward <none@none>
2017-06-14 21:46:21 +0100

    Add test app.

---
+++ TestApp/TestApp/Class1.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+
+Public Class Class1
+
+    Public Class SomeEventArgs
+        Inherits EventArgs
+
+    End Class
+
+    Public Event SomeOtherEvent As EventHandler(Of SomeEventArgs)
+
+    Public Sub AddValueChangedHandler(handler As EventHandler(Of SomeEventArgs))
+        AddHandler SomeOtherEvent, handler
+    End Sub
+
+End Class
+
